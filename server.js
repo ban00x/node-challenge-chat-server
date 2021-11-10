b6b25f465e679e19ba88e8aaa00936becedfa233
¨ban00x¨ <houssam.r321@gmail.com>
2021-11-10 19:12:10 +0100

    completed node part

--- server.js
+++ server.js
@@ -1,25 +1,126 @@
 const express = require("express");
 const cors = require("cors");
-
+const fs = require("fs");
+const { response, request } = require("express");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
+const saveNewMessageToJson = (messages) => {
+  const text = JSON.stringify(messages, null, 4);
+  fs.writeFileSync("./messages.json", text);
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+const getMessagesFromJson = () => {
+  const text = fs.readFileSync("./messages.json");
+  const obj = JSON.parse(text);
+  return obj;
+};
 
-app.get("/", function (request, response) {
+const fromHtml = (request, response) => {
   response.sendFile(__dirname + "/index.html");
-});
+};
 
-app.listen(3000, () => {
-   console.log("Listening on port 3000")
-  });
+//get all the messages
+const getAllMessages = (request, response) => {
+  const messages = getMessagesFromJson();
+  response.send(messages);
+};
+
+//get messages by Id
+const getMassagesById = (request, response) => {
+  const messagesId = parseInt(request.params.id);
+  let foundMessage = getMessagesFromJson().find((m) => m.id === messagesId);
+  if (foundMessage) {
+    response.status(201).send(foundMessage);
+  } else {
+    res.status(404).send("This message does not exist");
+  }
+};
+
+// get last 10 messages
+const getLastTenMessages = (request, response) => {
+  const lastTenMessages = getMessagesFromJson().slice(-10);
+  response.send(lastTenMessages);
+};
+
+//Create a new message with date
+const addNewMessageWithDate = (request, response) => {
+  const newMessage = request.body;
+  console.log(newMessage);
+  const messages = getMessagesFromJson();
+
+  const maxMessageId = Math.max(...messages.map((m) => m.id));
+  newMessage.id = maxMessageId + 1;
+  newMessage.timeSent = `${new Date()}`;
+
+  if (isEmpty(newMessage.text) || isEmpty(newMessage.from)) {
+    response.status(400).send("either text or sender name is empty");
+  } else {
+    messages.push(newMessage);
+    saveNewMessageToJson(messages);
+
+    response.status(201).send(newMessage);
+  }
+};
+
+const isEmpty = (v) => {
+  return v === "" || v === null || v === undefined;
+};
+
+//Delete a message, by ID
+const deleteMessageById = (request, response) => {
+  const messageId = parseInt(request.params.id);
+  let messages = getMessagesFromJson();
+
+  const jsonMessage = messages.find((m) => m.id === messageId);
+  if (jsonMessage) {
+    messages = messages.filter((m) => m.id != messageId);
+    saveNewMessageToJson(messages);
+    response.status(200).send(jsonMessage);
+  } else {
+    response.status(404).send("Did not find messsages with id " + messageId);
+  }
+};
+
+// search specific message with the text
+const searchSpecificMessageWithText = (request, response) => {
+  const query = request.query.text.toLowerCase();
+  const foundMessages = getMessagesFromJson().filter(
+    (m) => m.text.toLowerCase().includes(query) || m.from.toLowerCase(query)
+  );
+  if (foundMessages.length < 1) {
+    response.status(404).send(`Message doesn't contain "${query}"`);
+  } else {
+    response.send(foundMessages);
+  }
+};
+
+// change messages information
+const changeMessagesInfo = (request, response) => {
+  const id = parseInt(request.params.id);
+  const updatedInfo = request.body;
+  let foundMessage = getMessagesFromJson().find((m) => m.id === id);
+  if (updatedInfo.text) {
+    foundMessage.text = updatedInfo.text;
+  }
+  if (updatedInfo.from) {
+    foundMessage.from = updatedInfo.from;
+  }
+  addToJson(messages);
+  response.status(200).send(updatedInfo);
+};
+
+app.get("/", fromHtml);
+app.get("/messages", getAllMessages);
+app.post("/messages", addNewMessageWithDate);
+app.get("/messages/search", searchSpecificMessageWithText);
+app.patch("/messages/:id", changeMessagesInfo);
+app.get("/messages/latest", getLastTenMessages);
+app.delete("/messages/:id", deleteMessageById);
+app.get("/messages/:id", getMassagesById);
+
+app.listen(4000, () => {
+  console.log("Listening on port 4000");
+});
